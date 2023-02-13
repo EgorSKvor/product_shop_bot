a237a69d4fad17d56417f9d85676379ad03af434
Egor <ee3068316@gmail.com>
2023-02-13 13:21:23 +0300

    fixed id

--- tets.py
+++ tets.py
@@ -9,7 +9,7 @@ db = db_api.Database('db_api/database/shop_database.db')
 
 db.add_item(id=1, name='Тыква', count=10, photo_path=r'db_api/database/product_photo/pumpkin.jpeg')
 db.add_item(id=2, name='Помидор', count=45, photo_path=r'db_api/database/product_photo/tomat.jpg')
-db.add_item(id=2, name='Огурец', count=30, photo_path=r'db_api/database/product_photo/cucumber.jpg')
+db.add_item(id=3, name='Огурец', count=30, photo_path=r'db_api/database/product_photo/cucumber.jpg')
 
 
 print(db.select_all_items())
